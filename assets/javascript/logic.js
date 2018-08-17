49e32f6f7886815e95646f3ea532d004b75cb730
Shant Kevorkian <shant.kevorkian@gmail.com>
2018-08-17 11:44:50 -0700

    new api from google with new project, billing set up and api secured

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -124,7 +124,7 @@ function callback(results, status) {
             + isOpen + "</p>");
 
         $.ajax({
-            url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=AIzaSyCsIII7nDoZBxomgC4wF6ofXmAPN7pTr5w',
+            url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=AIzaSyD2xSa6BNQRyT5tJFK6Hhf6RPJzGrN35BI',
             method: "GET",
             success: function(response) {
                 console.log(response);
